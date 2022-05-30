9960f66008a8e85ca675253ea2117bdb05ce8b05
Raeann Gilgeous <raeann.gilgeous18@gmail.com>
2022-05-29 20:07:06 -0500

    finished challenge

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,28 +1,70 @@
+--DELIVERABLE 1
+
 SELECT employees.emp_no,
 		employees.first_name,
 		employees.last_name,
 		titles.title,
 		titles.from_date,
 		titles.to_date
-INTO retiring_employees_by_title
+INTO retirement_titles
 FROM employees
-INNER JOIN titles
-ON employees.emp_no = titles.emp_no
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	INNER JOIN titles
+		ON employees.emp_no = titles.emp_no
+WHERE (employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
 
-select * from retiring_employees_by_title
-
+select * from retirement_titles
 
 
 -- using the distinct formular to remove duplicate rows
-SELECT DISTINCT ON (rebt.emp_no) rebt.emp_no,
-rebt.first_name,
-rebt.last_name,
-rebt.title
 
-INTO unique_employees_by_title
-FROM retiring_employees_by_title AS rebt
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles AS rt
 ORDER BY emp_no,to_date DESC;
 
-select * from unique_employees_by_title
+select * from unique_titles
+
+
+
+-- number of titles from unique titles table
+
+SELECT COUNT (ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM retiring_titles
+
+
+
+
+
+--DELIVERABLE 2
+
+--ELIGIBILITY TO PARTICIPATE IN A MENTORSHIP PROGRAM
+
+
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+			INNER JOIN titles as t
+				ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibility
